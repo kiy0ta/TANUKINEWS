08b67b45fd29e94edf60f96eb4e4797a2a9bba89
kiyota <m-kiyota@cries.co.jp>
2020-08-10 21:00:09 +0900

    # firebase cosoleからバックグラウンドで通知を受け取る処理を追加  - 通知はくるが、アイコンのみ表示される

--- app/src/main/java/com/example/tanukinews/MainActivity.kt
+++ app/src/main/java/com/example/tanukinews/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.tanukinews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w("getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+                    Log.d("token:", token)
+                })
     }
 }
